5f64077ae9919594dedc1aed3e7a412a22560c3f
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2023-09-28 22:19:00 -0700

    added imports and def winner in game.py to check if a move made me a W

--- game.py
+++ game.py
@@ -1,3 +1,7 @@
+import math
+import time
+from player import HumanPlayer, RandomComputerPlayer, SmartComputorPlayer 
+
 class TicTacToe():
     # func to initialize the board n a winner which is set to None
     def __init__(self):
@@ -41,4 +45,33 @@ class TicTacToe():
             return True
         return False
 
+    # basically a func to check if a move made me a W or no
+    def winner(self, square, letter):
+        # check the row
+        row_ind = math.floor(square / 3)
+        row = self.board[row_ind*3:(row_ind+1)*3]
+        # print('row', row)
+        # so if all letters are the same its a W
+        if all([s == letter for s in row]):
+            return True
         
+        # now check all the letter in the column
+        col_ind = square % 3
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        # print('col', column)
+        if all([s == letter for s in column]):
+            return True
+        
+        # to check the diagonal 
+        # 1. check if square is even
+        # 2. if they are then i gonna take diagonal squares such as [0, 4, 8] or [2, 4, 6]
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            # print('diag1', diagonal1)
+            if all([s == letter for s in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            # print('diag2', diagonal2)
+            if all([s == letter for s in diagonal2]):
+                return True
+        return False
