6c25b4a6e4067b7a2bd19fcf6b5d6cce0abb7c80
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2023-09-29 11:28:13 -0700

    player.py update on classes and start working on minimax algo

--- player.py
+++ player.py
@@ -10,3 +10,47 @@ class Player():
     def get_move(self, game):
         pass
 
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+           #takes in human input, returns it into the integer n return that value
+           square = input(self.letter + '\'s turn, Input move (0-9): ')
+           try:
+               val = input(square)
+               if val not in game.avaliable_moves():
+                   raise ValueError
+               valid_square = True
+           except ValueError:
+                print('Invalid square. Try again.')
+        return val
+
+
+# this type of player just sees what square is avaliable on the board
+class RandomPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+         square = random.choice(game.avaliable_moves())
+         return square
+
+
+
+class SmartComputerPlayer(Player):
+        def __init__(self, letter):
+            super().__init__(letter)
+
+        def get_move(self, game):
+            # if its an empty game it randomly chooses somewhere to go
+            if len(game.available_moves()) == 9:
+                square = random.choice(game.available_moves())
+            else:
+                #otherwise uses minimax algorithm in order to generate the smartest possible choice at that moment
+                square = self.minimax(game, self.letter)['position']
+            return square  
+
